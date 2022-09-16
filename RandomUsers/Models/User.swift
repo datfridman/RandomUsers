c56a19c54a7a2c9683f685b57749c3d9df933b66
Leo Fridman <madfridman@gmail.com>
2022-09-16 13:37:50 +0200

    Random User App

---
+++ RandomUsers/Models/User.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+// MARK: - User
+struct User: Decodable {
+    let results: [Result]?
+    let info: Info?
+}
+
+// MARK: - Info
+struct Info: Decodable {
+    let seed: String?
+    let results, page: Int?
+    let version: String?
+}
+
+// MARK: - Result
+struct Result: Decodable {
+    let gender: String?
+    let name: Name?
+    let location: Location?
+    let email: String?
+    let login: Login?
+    let dob, registered: Dob?
+    let phone, cell: String?
+    let id: ID?
+    let picture: Picture?
+    let nat: String?
+}
+
+// MARK: - Dob
+struct Dob: Decodable {
+    let date: String?
+    let age: Int?
+}
+
+// MARK: - ID
+struct ID: Decodable {
+    let name, value: String?
+}
+
+// MARK: - Location
+struct Location: Decodable {
+    let street: Street?
+    let city, state, country: String?
+    let postcode: Int?
+    let coordinates: Coordinates?
+    let timezone: Timezone?
+}
+
+// MARK: - Coordinates
+struct Coordinates: Decodable {
+    let latitude, longitude: String?
+}
+
+// MARK: - Street
+struct Street: Decodable {
+    let number: Int?
+    let name: String?
+}
+
+// MARK: - Timezone
+struct Timezone: Decodable {
+    let offset, timezoneDescription: String?
+}
+
+// MARK: - Login
+struct Login: Decodable {
+    let uuid, username, password, salt: String?
+    let md5, sha1, sha256: String?
+}
+
+// MARK: - Name
+struct Name: Decodable {
+    let title, first, last: String?
+}
+
+// MARK: - Picture
+struct Picture: Decodable {
+    let large, medium, thumbnail: String?
+}
---
+++ RandomUsers/ViewControllers/MainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  RandomUsers
+//
+//  Created by Leon Fridman on 9/16/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    private let userDataURL = "https://randomuser.me/api/"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchUserData()
+    }
+
+}
+
+// MARK: - Networking
+extension MainViewController {
+    private func fetchUserData() {
+        guard let userURL = URL(string: userDataURL) else { return }
+        
+        let session = URLSession(configuration: .default)
+        
+        let getUserDataTask = session.dataTask(with: userURL) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "Error with no data")
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let user = try jsonDecoder.decode(User.self, from: data)
+                print(user)
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        }
+        getUserDataTask.resume()
+    }
+}
